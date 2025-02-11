c0ee30b23a50e0f06903ea8fd8eecfc1f8046c4a
ctliu5 <ctliu5@gmail.com>
2025-02-11 17:50:39 +0800

    Update deploy_single_redis_stack.sh

--- deploy_single_redis_stack.sh
+++ deploy_single_redis_stack.sh
@@ -9,7 +9,7 @@
 
 # ------------------ 使用者可調整參數 ------------------
 CONTAINER_NAME="redis-stack"
-IMAGE_NAME="redis-stack:7.4.0-v1"
+IMAGE_NAME="redis/redis-stack:7.4.0-v1"
 LOG_DIR="$(pwd)/logs"    # 預設使用目前目錄下的logs
 REMOVE_OLD_CONTAINER="false"
 
